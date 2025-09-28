265cd6d3748734b8eb96db8615c6f80d7ce2842b
Morgan Mulholland <morgan.p.mulholland@gmail.com>
2025-09-28 05:12:37 +0100

    Added example implementation for copy pasta

--- Assets/Scripts/Interfaces/IAPILoader.cs
+++ Assets/Scripts/Interfaces/IAPILoader.cs
@@ -3,7 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public interface IAPILoader
 {
+    //Example Implementation
+    /*
+        public void AddAPI(Lua lua)
+        {    
+            new LuaAPI(lua, "shell")
+                .RegisterFunction("clear", this, nameof(Clear))
+                .RegisterFunction("clearLine", this, nameof(ClearLine))     
+
+            // Global functions outside the shell table
+            lua.RegisterFunction("globalName", this, typeof(Shell).GetMethod(nameof(SomeFunction)));    
+        }
+     */
     public void AddAPI(Lua lua);
 }
