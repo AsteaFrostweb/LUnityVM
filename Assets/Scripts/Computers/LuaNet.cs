27c3bab71767e3c2dd7739cb7a3b4a70b26c0f03
Morgan Mulholland <morgan.p.mulholland@gmail.com>
2025-10-01 06:40:30 +0100

    Fixed Bug: Receive event never returning a value Fixed Bug: Sending C# object to lua enviroment instead of LuaTable

--- Assets/Scripts/Computers/LuaNet.cs
+++ Assets/Scripts/Computers/LuaNet.cs
@@ -1,4 +1,5 @@
 using NLua;
+using static System.Convert;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -6,11 +7,16 @@ public class LuaNet : IAPILoader
 {
     private Computer host;
 
+    public LuaNet(Computer _host) 
+    {
+        host = _host;
+    }
+
     public void AddAPI(Lua lua)
     {
-        new LuaAPI(lua, "LNet")
+        new LuaAPI(lua, "lnet")
            .RegisterFunction("send", this, nameof(Send))
-           .RegisterFunction("receive", this, nameof(Receive));
+           .RegisterFunction("receive", this, nameof(ReceiveExposed));
     }    
 
     //Adds a "luanet" ComputerEvent to the computer with a given ID
@@ -34,13 +40,23 @@ public class LuaNet : IAPILoader
 
         
         //Add a "luanet_receive" event to the receiver computers event system.
-        receiver.eventSystem.AddEvent(new ComputerEvent("luanet_receive", port, data, null, 1f));
+        receiver.eventSystem.AddEvent(new ComputerEvent("luanet_receive", port, data, host.ID, 5f));
 
         //Add a "luanet_send" event to the host's event system.
-        host.eventSystem.AddEvent(new ComputerEvent("luanet_send", port, data, null, 1f));
+        host.eventSystem.AddEvent(new ComputerEvent("luanet_send", port, data, null, 2f));
     }
 
-    private object Receive(int port, float timeout) 
+
+
+    private LuaTable ReceiveExposed(int port, double timeout) 
+    {
+        ComputerEvent ce = Receive(port, ToSingle(timeout));
+        if (ce == null)
+            return null;
+        
+        return LuaAPI.ComptuerEventToTable(host.currentShell.enviroment, ce);
+    }
+    private ComputerEvent Receive(int port, float timeout) 
     {
         float elapsed = 0f;
         ComputerEvent _event = null;
@@ -55,7 +71,7 @@ public class LuaNet : IAPILoader
         while (elapsed < timeout && _event == null)
         {
             //Attempt to get event of type luanet_receiv on the given port
-            _event = host.eventSystem.PullEvent("luanet_receive", c => (int)c.data2 == port);
+            _event = host.eventSystem.PullEvent("luanet_receive", c => (int)c.data1 == port);
 
             //Continuer if the event was found
             if (_event != null)
@@ -65,7 +81,9 @@ public class LuaNet : IAPILoader
             host.currentShell.Sleep(waitTime);
             elapsed += waitTime;
 
-        }        
+        }
+
+        if (_event == null) return null;
 
         return _event;
     }
